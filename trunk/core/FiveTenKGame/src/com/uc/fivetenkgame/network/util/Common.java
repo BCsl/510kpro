3c562fb4f1931bf0a81dd5d96a292031281ab7f2
eason.fu1994@gmail.com <eason.fu1994@gmail.com@e7340599-4d99-586b-4226-47d8824597a0>
2014-07-17 06:47:52 +0000

    修改了common中的通信格式

--- trunk/core/FiveTenKGame/src/com/uc/fivetenkgame/network/util/Common.java
+++ trunk/core/FiveTenKGame/src/com/uc/fivetenkgame/network/util/Common.java
@@ -8,34 +8,33 @@ package com.uc.fivetenkgame.network.util;
  */
 public class Common {
 
-	//玩家序号
-	public static final int SERVER_NUM	= 1;
+	// 玩家序号
+	public static final int SERVER_NUM = 1;
 	public static final int PLAYER1_NUM = 2;
 	public static final int PLAYER2_NUM = 3;
-	
+
 	//网络消息头标号定义
-	public static final String PLAYER_ACCEPTED = "1#";//连接成功信号：1#+玩家序号
-	public static final String PLAYER_REFUSED = "2#";//连接失败信号
+	public static final String PLAYER_ACCEPTED = "1#";//连接成功信号：1#+当前玩家数
+	public static final String PLAYER_REFUSED = "2#";//连接失败信号:2#
 	public static final String BEGIN_GAME = "3#";//游戏开始信号：3#+玩家序号+牌号+ ，+牌号....
-	public static final String YOUR_TURN = "4#";//提醒玩家出牌信号
+	public static final String YOUR_TURN = "4#";//提醒玩家出牌信号：4#+玩家序号
 	public static final String PLAY_END = "5#";//出牌转发信号：5#+玩家号+牌号 + , + .... 
 												//+桌面分数+玩家1牌数+玩家2牌数+玩家3牌数
 	public static final String ROUND_END = "6#";//回合结束信号，更新玩家的分数：6#+玩家1分数+玩家2分数+玩家3分数
 	public static final String GAME_OVER = "7#";//游戏结束信号：7#+胜利者
-	public static final String WINNING_PLAYER = "8#";
+	public static final String WINNING_PLAYER = "8#";//
 	
-	public static final String PLAY_CARDS = "9#";//自己出牌：12#+牌的序号 + , +.....	
-	public static final String GIVE_UP = "0#";//自己放弃出牌
+	public static final String PLAY_CARDS = "9#";//自己出牌：12#+玩家号+牌的序号 + , +.....	
+	public static final String GIVE_UP = "0#";//自己放弃出牌0#+玩家号
 	
-	//handler消息处理编号
+	// handler消息处理编号
 	public static final int UPDATE_WAITING_PLAYER_NUM = 1;
 	public static final int START_GAME = 2;
-	
-	
-	//服务器状态改变内部消息
+
+	// 服务器状态改变内部消息
 	public static final String SERVER_LISTENING = "1@";
 	public static final String GAME_START = "2@";
 	public static final String GAME_END = "3@";
-	//总玩家人数
+	// 总玩家人数
 	public static final int TOTAL_PLAYER_NUM = 3;
 }
