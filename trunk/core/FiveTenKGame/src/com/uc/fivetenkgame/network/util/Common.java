8184cabb32ee8d9f5ef080e4f4c020c9623b8fe4
cslgogogo@gmail.com <cslgogogo@gmail.com@e7340599-4d99-586b-4226-47d8824597a0>
2014-07-17 09:53:54 +0000

    增加 src/com/uc/fivetenkgame/state/serverstate/WaitingState.java

--- trunk/core/FiveTenKGame/src/com/uc/fivetenkgame/network/util/Common.java
+++ trunk/core/FiveTenKGame/src/com/uc/fivetenkgame/network/util/Common.java
@@ -24,8 +24,7 @@ public class Common {
 	public static final String PLAYER_REFUSED = "2#";//连接失败信号:2#
 	public static final String BEGIN_GAME = "3#";//游戏开始信号：3#+玩家序号+牌号+ ，+牌号....
 	public static final String YOUR_TURN = "4#";//提醒玩家出牌信号：4#+玩家序号
-	public static final String PLAY_END = "5#";//出牌转发信号：5#+玩家号+牌号 + , + .... 
-												//+桌面分数+玩家1牌数+玩家2牌数+玩家3牌数
+	public static final String PLAY_END = "5#";//出牌转发信号：5#+玩家号+牌号 + , + .... //+桌面分数+玩家1牌数+玩家2牌数+玩家3牌数（后4位）
 	public static final String ROUND_END = "6#";//回合结束信号，更新玩家的分数：6#+玩家1分数+玩家2分数+玩家3分数
 	public static final String GAME_OVER = "7#";//游戏结束信号：7#+胜利者
 	public static final String WINNING_PLAYER = "8#";//
