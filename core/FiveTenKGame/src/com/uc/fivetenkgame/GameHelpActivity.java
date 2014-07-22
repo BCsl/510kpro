6dcc18ee89133d01cd81a079c08e71b2dd51c37e
maxlzd24@gmail.com <maxlzd24@gmail.com@e7340599-4d99-586b-4226-47d8824597a0>
2014-07-22 09:26:38 +0000

    完善设置和帮助界面

--- core/FiveTenKGame/src/com/uc/fivetenkgame/GameHelpActivity.java
+++ core/FiveTenKGame/src/com/uc/fivetenkgame/GameHelpActivity.java
@@ -4,6 +4,12 @@ import my.example.fivetenkgame.R;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 帮助界面显示游戏规则
+ * 
+ * @author liuzd
+ *
+ */
 public class GameHelpActivity extends Activity {
 
 	@Override
