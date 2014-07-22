f096b89a7b99267f7f597f8b63a5a4ced7e6aeb4
maxlzd24@gmail.com <maxlzd24@gmail.com@e7340599-4d99-586b-4226-47d8824597a0>
2014-07-22 09:26:38 +0000

    完善设置和帮助界面

--- trunk/core/FiveTenKGame/src/com/uc/fivetenkgame/GameSettingActivity.java
+++ trunk/core/FiveTenKGame/src/com/uc/fivetenkgame/GameSettingActivity.java
@@ -2,15 +2,64 @@ package com.uc.fivetenkgame;
 
 import my.example.fivetenkgame.R;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 
+/**
+ * 设置界面类
+ * 
+ * 通过SharedPreferences设置是否打开音效及音量大小
+ * 
+ * @author liuzd
+ *
+ */
 public class GameSettingActivity extends Activity {
 
+	private SharedPreferences mSharedPreference;
+	private RadioButton mMusicOpenButton;
+	private RadioButton mMusicCloseButton;
+	private SeekBar mMusicVolume;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
 		
+		mMusicOpenButton = (RadioButton)findViewById(R.id.open_radio_id);
+		mMusicCloseButton = (RadioButton)findViewById(R.id.close_radio_id);
+		mMusicVolume = (SeekBar)findViewById(R.id.music_volume_seekBar_id);
+		
+		mSharedPreference = getSharedPreferences("five_ten_k_game", Activity.MODE_PRIVATE);
+		
+		//获得设置属性设置控件默认值
+		boolean isOpenMusic = mSharedPreference.getBoolean("isOpenMusic", true);
+		int volume = mSharedPreference.getInt("musicVolume", 50);
+		
+		mMusicVolume.setProgress(volume);
+		if( isOpenMusic ){
+			mMusicOpenButton.setChecked(true);
+		}
+		else{
+			mMusicCloseButton.setChecked(true);
+		}
 	}
 
+	@Override
+	protected void onPause() {
+
+		//保存设置
+		SharedPreferences.Editor editor = mSharedPreference.edit();
+		editor.clear();
+		
+		editor.putBoolean("isOpenMusic", mMusicOpenButton.isChecked());
+		editor.putInt("musicVolume", mMusicVolume.getProgress());
+		
+		editor.commit();
+		
+		super.onPause();
+	}
+
+	
 }
