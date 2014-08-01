7a80da30f1e942eb170c88076ad530f5cbd003c4
eason.fu1994@gmail.com <eason.fu1994@gmail.com@e7340599-4d99-586b-4226-47d8824597a0>
2014-08-01 07:30:11 +0000

    调整了一些方法的返回类型，修改了一些bug

--- core/FiveTenKGame/src/com/uc/fivetenkgame/common/SharePreferenceCommon.java
+++ core/FiveTenKGame/src/com/uc/fivetenkgame/common/SharePreferenceCommon.java
@@ -15,5 +15,6 @@ public class SharePreferenceCommon {
 	//用户名历史记录表
 	public static final String TABLE_HISTORY="history";
 	public static final String FIELD_HISTORY="history";
-
+	//各玩家输赢记录
+	public static final String MONEY_RECORD="money";
 }
