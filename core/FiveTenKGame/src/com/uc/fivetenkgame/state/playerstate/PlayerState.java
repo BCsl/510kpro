2ca97c1c94ae9fc366433e03a83bc20d55241cf3
linming1247576468@gmail.com <linming1247576468@gmail.com@e7340599-4d99-586b-4226-47d8824597a0>
2014-08-04 07:40:31 +0000

    删除多余的exitState

--- core/FiveTenKGame/src/com/uc/fivetenkgame/state/playerstate/PlayerState.java
+++ core/FiveTenKGame/src/com/uc/fivetenkgame/state/playerstate/PlayerState.java
@@ -3,16 +3,23 @@ package com.uc.fivetenkgame.state.playerstate;
 import com.uc.fivetenkgame.player.PlayerContext;
 import com.uc.fivetenkgame.state.State;
 
-public abstract class PlayerState implements State{
+/**
+ * player状态的父类抽象类，引入PlayerContext作为状态与player交互的接口
+ * 
+ * @author lm
+ * 
+ */
+public abstract class PlayerState implements State {
 
-	protected PlayerContext mPlayerContext;
-	
-	public PlayerState(PlayerContext context){
-		this.mPlayerContext = context;
-	}
-	@Override
-	public String toString() {
-		String name=this.getClass().getName();
-		return "当前状态："+name.substring(name.lastIndexOf("."));
-	}
+    protected PlayerContext mPlayerContext;
+
+    public PlayerState(PlayerContext context) {
+        this.mPlayerContext = context;
+    }
+
+    @Override
+    public String toString() {
+        String name = this.getClass().getName();
+        return "当前状态：" + name.substring(name.lastIndexOf("."));
+    }
 }
--- core/FiveTenKGame/src/com/uc/fivetenkgame/state/playerstate/WaitForMsgState.java
+++ core/FiveTenKGame/src/com/uc/fivetenkgame/state/playerstate/WaitForMsgState.java
@@ -7,47 +7,50 @@ import com.uc.fivetenkgame.common.NetworkCommon;
 import com.uc.fivetenkgame.player.PlayerContext;
 
 /**
+ * 游戏进行时的主要状态，等待出牌，更新等信息
  * 
  * @author fuyx ,chensl@ucweb.com,lm
  * 
- * 
  */
 public class WaitForMsgState extends PlayerState {
-	String tag = "WaitForMsgState";
+    String tag = "player WaitForMsgState";
 
-	public WaitForMsgState(PlayerContext player) {
-		super(player);
-	}
+    public WaitForMsgState(PlayerContext player) {
+        super(player);
+    }
 
-	@Override
-	public void handle(String msg) {
-	    Log.i(tag,"msg is " + msg);
-		if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.PLAYER_STATE_CHANGE)) {//由上一状态跳转而来 
-			
-		}else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.YOUR_TURN)) {//轮到某个玩家
-		    int playerNumber = CommonMsgDecoder.getPlayerNumber(msg);
+    @Override
+    public void handle(String msg) {
+        Log.i(tag, "msg is " + msg);
+        if (CommonMsgDecoder.checkMessage(msg,
+                NetworkCommon.PLAYER_STATE_CHANGE)) {// 由上一状态跳转而来
+
+        } else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.YOUR_TURN)) {// 轮到某个玩家
+            int playerNumber = CommonMsgDecoder.getPlayerNumber(msg);
             mPlayerContext.setCurrentPlayer(playerNumber);
             // 自己出牌
             if (playerNumber == mPlayerContext.getPlayerNumber()) {
                 mPlayerContext.setMyTurn(true);
-                
+
                 mPlayerContext.setState(new PlayCardState(mPlayerContext));
                 mPlayerContext.handle(NetworkCommon.PLAYER_STATE_CHANGE);
             }
-        } else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.GIVE_UP)) {//其他玩家放弃出牌
+        } else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.GIVE_UP)) {// 其他玩家放弃出牌
             mPlayerContext.playerGiveUp(CommonMsgDecoder.getPlayerNumber(msg));
 
-        } else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.PLAY_END)) {//其他玩家成功出牌
-            mPlayerContext.playCardsEndAction(
-                    CommonMsgDecoder.getCards(msg),
+        } else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.PLAY_END)) {// 其他玩家成功出牌
+            mPlayerContext.playCardsEndAction(CommonMsgDecoder.getCards(msg),
                     String.valueOf(CommonMsgDecoder.getPlayerNumber(msg)),
                     String.valueOf(CommonMsgDecoder.getTableScore(msg)),
                     CommonMsgDecoder.getRemainCardNumbers(msg));
+            
         } else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.ROUND_END)) {// 得到回合结束信息
-            mPlayerContext.roundEndAction(CommonMsgDecoder.getPlayerScores(msg));
+            mPlayerContext
+                    .roundEndAction(CommonMsgDecoder.getPlayerScores(msg));
+            
         } else if (CommonMsgDecoder.checkMessage(msg, NetworkCommon.GAME_OVER)) { // 得到游戏结束信息
             mPlayerContext.setState(new GameOverState(mPlayerContext));
             mPlayerContext.handle(msg);
-        } 
+        }
     }
 }
